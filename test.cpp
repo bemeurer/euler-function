2805ffdb3ba955ad357df4cd5d8840ce98007469
Bernardo Meurer <meurerbernardo@gmail.com>
2016-05-11 20:53:10 -0300

    Completed migration to GMP

--- test.cpp
+++ test.cpp
@@ -1,15 +1,11 @@
 #include "totient.hpp"
 #include <iostream>
 
-int main(void)
-{
-    mpz_class a;
-    a = "-195349285623987465324987562398476";
-    std::cout << phi(1).get_str(10) << "\n";
-    std::cout << phi(2).get_str(10) << "\n";
-    std::cout << phi(3).get_str(10) << "\n";
-    std::cout << phi(4).get_str(10) << "\n";
-    std::cout << phi(5).get_str(10) << "\n";
-    std::cout << phi(6).get_str(10) << "\n";
-    //std::cout << phi(a).get_str(10) << "\n";
+int main(void) {
+    mpz_class foo;
+    foo = "-195349285623987465324987562398476";
+    for (int a = 0; a <= 100; a++) {
+        //std::cout << phi(a).get_str(10) << "\n";
+    }
+    std::cout << phi(foo).get_str(10) << "\n";
 }
--- totient.hpp
+++ totient.hpp
@@ -2,72 +2,61 @@
 #include <gmpxx.h>
 
 
-mpz_class phi(mpz_class x){
-    if(mpz_cmp_d(x.get_mpz_t(), 0) < 0){
-        x*= -1;
+mpz_class phi(mpz_class x) {
+    // Just invert if negative
+    if (mpz_cmp_d(x.get_mpz_t(), 0) < 0) {
+        x *= -1;
     }
-    if (x == 0){
+
+    // Definition
+    if (x == 0) {
         return 0;
     }
-    if (mpz_cmp_d(x.get_mpz_t(), 2) <= 0){
+
+    // phi(1), phi(2) = 1
+    if (mpz_cmp_d(x.get_mpz_t(), 2) <= 0) {
         return 1;
     }
-    if (mpz_probab_prime_p(x.get_mpz_t(), 50))
-    {
+
+    // Lehmer's conjecture
+    if (mpz_probab_prime_p(x.get_mpz_t(), 50)) {
         x -= 1;
         return x;
     }
-    if(mpz_even_p(x.get_mpz_t())){
+    // Even number?
+    if (mpz_even_p(x.get_mpz_t())) {
         mpz_class y = x;
         y /= 2;
-        if(mpz_even_p(y.get_mpz_t())){
+        if (mpz_even_p(y.get_mpz_t())) {
             x = phi(y);
             x *= 2;
             return x;
         }
         return phi(y);
     }
+    // For all primes smaller than n
     primesieve::iterator pi;
     mpz_class prime;
     mpz_class sqrtx;
     mpz_sqrt(sqrtx.get_mpz_t(), x.get_mpz_t());
-    for (prime = pi.next_prime();mpz_cmp ; prime =pi.next_prime())
-    {//prime <= sqrtx
+    for (prime = pi.next_prime(); mpz_cmp; prime = pi.next_prime()) {
         mpz_class k = prime;
-        if(! mpz_divisible_p(x.get_mpz_t(), k.get_mpz_t()))
-        {
+        // Check if k is a factor
+        if (!mpz_divisible_p(x.get_mpz_t(), k.get_mpz_t())) {
             continue;
         }
         mpz_class o;
         mpz_div(o.get_mpz_t(), x.get_mpz_t(), k.get_mpz_t());
         mpz_class d = gcd(k, o);
-        if (d ==1){
+        if (d == 1) {
             mpz_mul(x.get_mpz_t(), phi(k).get_mpz_t(), phi(o).get_mpz_t());
             return (x);
-        }else{
+        } else {
             mpz_mul(x.get_mpz_t(), phi(k).get_mpz_t(), phi(o).get_mpz_t());
             mpz_class z;
             mpz_div(z.get_mpz_t(), x.get_mpz_t(), phi(d).get_mpz_t());
-            return(z);
+            return (z);
         }
     }
+
 }
-
-
-/*
-
-    primesieve::iterator pi;
-    cpp_int prime;
-    std::cout << "Starting prime loop\n";
-    for (prime = pi.next_prime(); prime <= boost::multiprecision::sqrt(x); prime=pi.next_prime())
-    {
-        cpp_int k = prime;
-        if (x % k) continue;
-        cpp_int o = x/k;
-        cpp_int d = boost::math::gcd(k, o);
-        if (d == 1) {
-            return phi(k) * phi(o);
-        }
-        return phi(k) * phi(o) * d / phi(d);
-    }
-}*/
