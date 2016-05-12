77bd60f94c61ce0563b3a027cbf4cd4f427ab67b
Bernardo Meurer <meurerbernardo@gmail.com>
2016-05-12 00:07:10 -0300

    Better braces

--- test.cpp
+++ test.cpp
@@ -6,34 +6,43 @@ bool test(mpz_class x, mpz_class y, int n = 50);
 
 bool rangeTest(bool verbose);
 
-int main(void) {
-    if (rangeTest(false)) {
+int main(void)
+{
+    if (rangeTest(false))
+    {
         std::cout << "phi(0) - phi(500) | OK";
     }
 }
 
-bool test(mpz_class x, mpz_class y, int n) {
+bool test(mpz_class x, mpz_class y, int n)
+{
     bool res = true;
-    for (int z = 1; z <= n; z++) {
+    for (int z = 1; z <= n; z++)
+    {
         res = (phi(x) == y);
     }
     return (res);
 }
 
-bool rangeTest(bool verbose) {
+bool rangeTest(bool verbose)
+{
     bool passed = true;
     io::CSVReader<2> in("phi_results.csv");
     in.read_header(io::ignore_extra_column, "n", "phi");
     int n;
     int phi;
-    while (in.read_row(n, phi)) {
-        if (test(n, phi, 10)) {
-            if (verbose) {
+    while (in.read_row(n, phi))
+    {
+        if (test(n, phi, 10))
+        {
+            if (verbose)
+            {
                 std::cout << "phi(" << n << ") = " << phi << " | OK\n";
             }
         }
         else {
-            if (verbose) {
+            if (verbose)
+            {
                 std::cout << "phi(" << n << ") = " << phi << " | FALSE\n";
             }
             passed = false;
--- totient.hpp
+++ totient.hpp
@@ -2,32 +2,39 @@
 #include <gmpxx.h>
 
 
-mpz_class phi(mpz_class x) {
+mpz_class phi(mpz_class x)
+{
     // Just invert if negative
-    if (mpz_cmp_d(x.get_mpz_t(), 0) < 0) {
+    if (mpz_cmp_d(x.get_mpz_t(), 0) < 0)
+    {
         x *= -1;
     }
 
     // Definition
-    if (x == 0) {
+    if (x == 0)
+    {
         return 0;
     }
 
     // phi(1), phi(2) = 1
-    if (mpz_cmp_d(x.get_mpz_t(), 2) <= 0) {
+    if (mpz_cmp_d(x.get_mpz_t(), 2) <= 0)
+    {
         return 1;
     }
 
     // Lehmer's conjecture
-    if (mpz_probab_prime_p(x.get_mpz_t(), 50)) {
+    if (mpz_probab_prime_p(x.get_mpz_t(), 50))
+    {
         x -= 1;
         return x;
     }
     // Even number?
-    if (mpz_even_p(x.get_mpz_t())) {
+    if (mpz_even_p(x.get_mpz_t()))
+    {
         mpz_class y = x;
         y /= 2;
-        if (mpz_even_p(y.get_mpz_t())) {
+        if (mpz_even_p(y.get_mpz_t()))
+        {
             x = phi(y);
             x *= 2;
             return x;
@@ -39,19 +46,23 @@ mpz_class phi(mpz_class x) {
     mpz_class prime;
     mpz_class sqrtx;
     mpz_sqrt(sqrtx.get_mpz_t(), x.get_mpz_t());
-    for (prime = pi.next_prime(); mpz_cmp(prime.get_mpz_t(), sqrtx.get_mpz_t()) <= 0; prime = pi.next_prime()) {
+    for (prime = pi.next_prime(); mpz_cmp(prime.get_mpz_t(), sqrtx.get_mpz_t()) <= 0; prime = pi.next_prime())
+    {
         mpz_class k = prime;
         // Check if k is a factor
-        if (!mpz_divisible_p(x.get_mpz_t(), k.get_mpz_t())) {
+        if (!mpz_divisible_p(x.get_mpz_t(), k.get_mpz_t()))
+        {
             continue;
         }
         mpz_class o;
         mpz_div(o.get_mpz_t(), x.get_mpz_t(), k.get_mpz_t());
         mpz_class d = gcd(k, o);
-        if (mpz_cmp_si(d.get_mpz_t(), 1) == 0) {
+        if (mpz_cmp_si(d.get_mpz_t(), 1) == 0)
+        {
             mpz_mul(x.get_mpz_t(), phi(k).get_mpz_t(), phi(o).get_mpz_t());
             return (x);
-        } else {
+        } else
+        {
             mpz_mul(x.get_mpz_t(), phi(k).get_mpz_t(), phi(o).get_mpz_t());
             mpz_class trans;
             mpz_mul(trans.get_mpz_t(), x.get_mpz_t(), d.get_mpz_t());
@@ -62,18 +73,21 @@ mpz_class phi(mpz_class x) {
     mpz_class p;
     for (p = 2 + (pi.previous_prime() - 1);
          mpz_cmp(p.get_mpz_t(), x.get_mpz_t()) < 0 && !mpz_divisible_p(x.get_mpz_t(), p.get_mpz_t()); p += 2);
-    if (mpz_cmp(p.get_mpz_t(), x.get_mpz_t()) >= 0) {
+    if (mpz_cmp(p.get_mpz_t(), x.get_mpz_t()) >= 0)
+    {
         x -= 1;
         return (x);
     }
     mpz_class o;
     mpz_div(o.get_mpz_t(), x.get_mpz_t(), p.get_mpz_t());
     mpz_class d = gcd(p, o);
-    if (mpz_cmp_si(d.get_mpz_t(), 1) == 0) {
+    if (mpz_cmp_si(d.get_mpz_t(), 1) == 0)
+    {
         mpz_mul(x.get_mpz_t(), phi(p).get_mpz_t(), phi(o).get_mpz_t());
         return (x);
     }
-    else {
+    else
+    {
         mpz_mul(x.get_mpz_t(), phi(p).get_mpz_t(), phi(o).get_mpz_t());
         mpz_class trans;
         mpz_mul(trans.get_mpz_t(), x.get_mpz_t(), d.get_mpz_t());
