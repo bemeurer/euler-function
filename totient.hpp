dd8a7084f1bda6028b8f12d95ec666d2c66b006b
Bernardo Meurer <meurerbernardo@gmail.com>
2016-05-11 23:19:01 -0300

    Fixed bugs

--- totient.hpp
+++ totient.hpp
@@ -39,7 +39,7 @@ mpz_class phi(mpz_class x) {
     mpz_class prime;
     mpz_class sqrtx;
     mpz_sqrt(sqrtx.get_mpz_t(), x.get_mpz_t());
-    for (prime = pi.next_prime(); mpz_cmp; prime = pi.next_prime()) {
+    for (prime = pi.next_prime(); mpz_cmp(prime.get_mpz_t(), sqrtx.get_mpz_t()) <= 0; prime = pi.next_prime()) {
         mpz_class k = prime;
         // Check if k is a factor
         if (!mpz_divisible_p(x.get_mpz_t(), k.get_mpz_t())) {
@@ -48,15 +48,36 @@ mpz_class phi(mpz_class x) {
         mpz_class o;
         mpz_div(o.get_mpz_t(), x.get_mpz_t(), k.get_mpz_t());
         mpz_class d = gcd(k, o);
-        if (d == 1) {
+        if (mpz_cmp_si(d.get_mpz_t(), 1) == 0) {
             mpz_mul(x.get_mpz_t(), phi(k).get_mpz_t(), phi(o).get_mpz_t());
             return (x);
         } else {
             mpz_mul(x.get_mpz_t(), phi(k).get_mpz_t(), phi(o).get_mpz_t());
-            mpz_class z;
-            mpz_div(z.get_mpz_t(), x.get_mpz_t(), phi(d).get_mpz_t());
-            return (z);
+            mpz_class trans;
+            mpz_mul(trans.get_mpz_t(), x.get_mpz_t(), d.get_mpz_t());
+            mpz_div(x.get_mpz_t(), trans.get_mpz_t(), phi(d).get_mpz_t());
+            return (x);
         }
     }
-
+    mpz_class p;
+    for (p = 2 + (pi.previous_prime() - 1);
+         mpz_cmp(p.get_mpz_t(), x.get_mpz_t()) < 0 && !mpz_divisible_p(x.get_mpz_t(), p.get_mpz_t()); p += 2);
+    if (mpz_cmp(p.get_mpz_t(), x.get_mpz_t()) >= 0) {
+        x -= 1;
+        return (x);
+    }
+    mpz_class o;
+    mpz_div(o.get_mpz_t(), x.get_mpz_t(), p.get_mpz_t());
+    mpz_class d = gcd(p, o);
+    if (mpz_cmp_si(d.get_mpz_t(), 1) == 0) {
+        mpz_mul(x.get_mpz_t(), phi(p).get_mpz_t(), phi(o).get_mpz_t());
+        return (x);
+    }
+    else {
+        mpz_mul(x.get_mpz_t(), phi(p).get_mpz_t(), phi(o).get_mpz_t());
+        mpz_class trans;
+        mpz_mul(trans.get_mpz_t(), x.get_mpz_t(), d.get_mpz_t());
+        mpz_div(x.get_mpz_t(), trans.get_mpz_t(), phi(d).get_mpz_t());
+        return (x);
+    }
 }
